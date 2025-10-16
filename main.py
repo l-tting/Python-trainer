5abf0328ad45668187b980ef1caac83187ec1e4b
l-tting <brianletting01@gmail.com>
2025-10-16 20:07:24 +0300

    lesson 2 -py

--- main.py
+++ main.py
@@ -2,6 +2,21 @@ x = 10
 number = 459
 email = "brian@gmail.com"
 full_name = "Jane Doe"
-print(x)
-print(number)
-print("Hello World")
+
+
+num1 = 15
+num2 = 81.01
+message = "Hello World!!"
+password_correct = False
+
+
+print(num1)
+print(type(num1))
+print(type(num2))
+print(type(message))
+print(type(password_correct))
+
+
+value = input("Type something: ")
+print(value)
+print(type(value))
