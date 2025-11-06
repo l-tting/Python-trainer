8ce1d887b60c7ee19ce2e873856078ffb3a1c1e4
l-tting <brianletting01@gmail.com>
2025-11-06 14:54:37 +0300

    ...

--- functions2.py
+++ functions2.py
@@ -11,13 +11,13 @@ def get_average(value):
     return value / 5
 
 def get_grade(avg):
-    if avg > 70 and avg < 100:
+    if avg > 79 and avg < 100:
         return "A"
-    elif avg > 60 and avg < 79:
+    elif avg >= 60 and avg <= 79:
         return "B"
-    elif avg > 50 and avg < 59:
+    elif avg >= 50 and avg <= 59:
         return "C"
-    elif avg > 40 and avg < 49:
+    elif avg >= 40 and avg <= 49:
         return "D"
     elif avg < 40:
         return "E"
@@ -26,9 +26,9 @@ def get_grade(avg):
 
 total_mark = get_total(maths,eng,swa,sci,sos)
 average = get_average(total_mark)
-# grade = get_grade(average)
+grade = get_grade(average)
 
 print("Total mark is: ",total_mark)
 print("Average mark is: ",average)
-# print("Final grade is: ",grade)
+print("Final grade is: ",grade)
 
